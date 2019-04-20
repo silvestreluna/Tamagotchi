3cf7d0a3c9b466194b9b18e0cd64d8dd39b06304
Silvestre Luna <slvtrluna@gmail.com>
2019-04-20 13:59:43 -0500

    wip

--- src/javascripts/components/eat.js
+++ src/javascripts/components/eat.js
@@ -2,21 +2,48 @@ import util from '../helpers/util';
 
 let full = 100;
 
-
 const addHealthyFood = () => {
-  full += 10;
+  const add = 10;
+
+  if (full >= 100) {
+    full = 100;
+    document.getElementById('eatButton').disabled = true;
+  } else if (full < 100) {
+    document.getElementById('eatButton').disabled = false;
+    const total = full + add;
+    console.error(total);
+    full = total;
+  }
+  util.printToDom('score', `<p>${full}</p>`);
 };
 
+const unhealthyFood = () => {
+  document.getElementById('eatButton').disabled = false;
+  const badFood = 3;
+  const total = full - badFood;
+  if (total <= 100) {
+    full = total;
+  } else if (total >= 100) {
+    full = 100;
+  }
+  util.printToDom('score', `<p>${full}</p>`);
+};
+
+
 const addEvent = () => {
-  const eventId = document.getElementById('eatButton');
-  eventId.addEventListener('click', addHealthyFood);
+  const goodFood = document.getElementById('eatButton');
+  const badFood = document.getElementById('badFood');
+  goodFood.addEventListener('click', addHealthyFood);
+  badFood.addEventListener('click', unhealthyFood);
 };
 
 const domBuilder = () => {
-  let domString = '<h3>Eat</h3>';
-  domString += `<p>${full}</p>`;
+  let domString = '<div>';
+  domString += '<h3>Eat</h3>';
+  domString += '<div id="score">100</div>';
   domString += '<button id="eatButton">Healthy Food</button>';
-  domString += '<button>Unhealthy food</button>';
+  domString += '<button id="badFood">Unhealthy food</button>';
+  domString += '</div>';
   util.printToDom('eat', domString);
   addEvent();
 };
